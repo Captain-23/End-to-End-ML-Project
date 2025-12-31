44ae0041197309663c79fadb6c592bbce6724235
Arnav Alok <arnavaalok@gmail.com>
2025-12-31 15:04:39 +0530

    Hyper parameter training added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,8 +45,48 @@ class ModelTrainer:
                 
             }
 
+            params = {
+                "Random Forest": {
+                    "n_estimators": [100, 200, 300],
+                    "max_depth": [None, 10, 20, 30],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4],
+                    "max_features": ["sqrt", "log2"]
+                },
+
+                "Decision Tree": {
+                    "max_depth": [None, 5, 10, 20, 30],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 5],
+                    "max_features": ["sqrt", "log2", None]
+                },
+
+                "Gradient Boosting": {
+                    "n_estimators": [100, 200, 300],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "max_depth": [3, 5, 7],
+                    "subsample": [0.8, 1.0],
+                    "min_samples_split": [2, 5],
+                    "min_samples_leaf": [1, 3]
+                },
+
+                "Linear Regressor": {},
+
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [3, 5, 7, 9, 11],
+                    "weights": ["uniform", "distance"],
+                    "metric": ["euclidean", "manhattan", "minkowski"]
+                },
+
+                "Adaboost Regressor": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1, 1.0],
+                    "loss": ["linear", "square", "exponential"]
+                }
+}
+
             model_report:dict=evaluate_models(X_train=X_train, y_train=y_train,X_test=X_test, y_test=y_test, 
-                                             models=models)
+                                             models=models, params=params)
             
             ## To get the best model score from dictionary
             best_model_score = max(sorted(model_report.values()))
